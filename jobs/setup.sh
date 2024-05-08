2786d3fc9fcc0df75e92a1b11e0bad309f120cea
abhaystoic <iabhaygupta90@gmail.com>
2024-05-08 23:25:21 +0530

    Celery command fix

--- jobs/setup.sh
+++ jobs/setup.sh
@@ -2,5 +2,5 @@
 export ENVIRONMENT=DEV
 mkdir -p ~/emotipress/celery/logs/
 mkdir -p ~/emotipress/celery-beat/logs/
-celery worker -A jobs.headlines.get_headlines --loglevel=info  -f ~/emotipress/logs/headlines-worker.log -Q headlines
+celery -A jobs.headlines.get_headlines worker --loglevel=info  -f ~/emotipress/logs/headlines-worker.log -Q headlines
 celery -A get_headlines beat --loglevel=info -f ~/emotipress/celery-beat/logs/headlines-beat.log
